1052aab683d46f5bb56bd4df048c1c379d9d4dbc
Louise <louise.beh@gmail.com>
2016-06-08 11:26:24 +0100

    Create angular function with dummy data for 2 days

--- app.js
+++ app.js
@@ -8,4 +8,36 @@ angular.module('scheduleApp', ['firebase'])
   var ref = new Firebase("https://scheduled-app.firebaseio.com/days");
   var fb = $firebase(ref);
 
+  // function to set the default data
+  $scope.reset = function() {
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+          name: 'Tuesday',
+          slots: {
+            0900: {
+              time: '9:00am',
+              booked: false
+            },
+            0110: {
+              time: '11:00am',
+              booked: false
+            }
+          }
+        }
+      });
+  };
 });
