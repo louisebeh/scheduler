dea7caf7eb5959af90ecadba57c4102a10fb0caf
Louise <louise.beh@gmail.com>
2016-06-08 13:35:05 +0100

    Fix typos and indentation

--- app.js
+++ app.js
@@ -19,25 +19,27 @@ angular.module('scheduleApp', ['firebase'])
             time: '9:00am',
             booked: false
           },
-          0110: {
+          1100: {
             time: '11:00am',
             booked: false
           }
         }
       },
       tuesday: {
-          name: 'Tuesday',
-          slots: {
-            0900: {
-              time: '9:00am',
-              booked: false
-            },
-            0110: {
-              time: '11:00am',
-              booked: false
-            }
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false
           }
         }
-      });
+      }
+    });
+
   };
+
 });
